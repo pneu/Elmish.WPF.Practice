2a948920e64b52afdeeb54f2bb7925779eecad47
pneu <pneumaster@gmail.com>
2020-09-22 11:19:01 +0900

    dont work

--- elmish.wpf.prac/Program.fs
+++ elmish.wpf.prac/Program.fs
@@ -4,26 +4,63 @@ open System
 open Elmish.WPF;
 open MyNamespace;
 
+type User = {
+    mutable IsSelected: bool
+    // IsSelected: bool
+    Name: string
+}
+
 type Model = {
     Count: int
     StepSize: int
+    Names: string list
+    Users: User list
 }
 
 let init () = {
     Count = 0
     StepSize = 1
+    Names = [
+        "foo"
+        "bar"
+        "puyo"
+        "huga"
+    ]
+    Users = [
+        { IsSelected = false; Name = "Alice" }
+        { IsSelected = true; Name = "Bob" }
+        { IsSelected = true; Name = "Charles" }
+    ]
 }
 
 type Msg =
     | Increment
     | Decrement
     | SetStepSize of int
+    // | Toggle of (User * bool)
+    | Ignore
+
+type SubMsg =
+    | ToggleSelected of bool
 
 let update msg m =
     match msg with
     | Increment -> { m with Count = m.Count + m.StepSize }
     | Decrement -> { m with Count = m.Count - m.StepSize }
     | SetStepSize x -> { m with StepSize = x }
+    | Ignore -> m
+    // | Toggle (user, v) ->
+    //     { m with Users = m.Users |> List.map (fun u ->
+    //                                             if user = u
+    //                                             then subUpdate (ToggleSelected v) u
+    //                                             else u) }
+
+let subUpdate msg sm =
+    match msg with
+    | ToggleSelected x ->
+        sm.IsSelected <- x
+        sm
+    // | ToggleSelected x -> { sm with IsSelected = x }
 
 let bindings () =
     [
@@ -32,17 +69,29 @@ let bindings () =
         "Decrement" |> Binding.cmd (fun m -> Decrement)
         "StepSize" |> Binding.twoWay (
             (fun m -> float m.StepSize),
-            (fun newVal m -> int newVal |> SetStepSize)
+            (fun newVal m -> int newVal |> SetStepSize))
+        "Names" |> Binding.oneWaySeq (
+            (fun m -> m.Names), (=), id)
+        "UserList" |> Binding.subModelSeq (
+            (fun m -> m.Users),
+            (fun sm -> sm.Name),
+            (fun _ -> Ignore),
+            (fun () -> [
+                "Name" |> Binding.oneWay (fun (_, user) -> user.Name)
+                // "IsSelected" |> Binding.twoWay (
+                //     (fun (_, user) -> user.IsSelected),
+                //     (fun value (_, user) -> Toggle (user, value)))
+                "IsSelected" |> Binding.twoWay (
+                    (fun (_, user) -> user.IsSelected),
+                    ToggleSelected)
+            ])
         )
     ]
-let emptyBindings () : Binding<Model, Msg> list = []
 
 [<EntryPoint; STAThread>]
 let main argv =
     Program.mkSimpleWpf init update bindings
-    // Program.mkSimpleWpf init update emptyBindings
-    |> Program.runWindow (MainWindow ())
-
-//let main window =
-//  Program.mkSimpleWpf (fun () -> init) update bindings
-//  |> Program.runWindow window
+    |> Program.withDebugTrace
+    |> Program.runWindowWithConfig {
+        ElmConfig.Default with LogConsole=true; LogTrace=true; Measure=true }
+        (MainWindow ())
